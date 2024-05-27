6aada15f4d9cde97693c3437af7b3773335a4864
mohitbooraget <137650003+mohitbooraget@users.noreply.github.com>
2024-05-27 15:13:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ async def account_login(bot: Client, m: Message):
              url =  "https://psitoffers.store/testkey.php?vid=" + id + "&quality=" + raw_text2
 
             name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
-            name = f'{str(count).zfill(3)}) {name1[:60]}'
+            name = f'{str(count).zfill(3)})〔«𝙼𝚎𝚗𝚍𝚊𝚡»〕{name1[:60]}'
 
             if "youtu" in url:
                 ytf = f"b[height<={raw_text2}][ext=mp4]/bv[height<={raw_text2}][ext=mp4]+ba[ext=m4a]/b[ext=mp4]"
